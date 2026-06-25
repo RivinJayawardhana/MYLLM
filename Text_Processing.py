4657ece496680b12defea4e44c5fa34309139944
Rivin <it22061652@my.sliit.lk>
2026-06-26 01:05:31 +0530

    feat : text processing functions

---
+++ Text_Processing.py
@@ -0,0 +1,86 @@
+
+import re
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab            #1
+        self.int_to_str = {i:s for s,i in vocab.items()}        #2
+
+    def encode(self, text):         #3
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+
+    def decode(self, ids):         #4
+        text = " ".join([self.int_to_str[i] for i in ids]) 
+
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)    #5
+        return text
+    
+
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = { i:s for s,i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        preprocessed = [item if item in self.str_to_int            #1
+                        else "<|unk|>" for item in preprocessed]
+
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)    #2
+        return text
+    
+
+
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(txt)    #1
+
+        for i in range(0, len(token_ids) - max_length, stride):     #2
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):    #3
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):         #4
+        return self.input_ids[idx], self.target_ids[idx]
+
+
+def create_dataloader_v1(txt, batch_size=4, max_length=256,
+                         stride=128, shuffle=True, drop_last=True,
+                         num_workers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")                         #1
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)   #2
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,     #3
+        num_workers=num_workers     #4
+    )
+
+    return dataloader
+
+
